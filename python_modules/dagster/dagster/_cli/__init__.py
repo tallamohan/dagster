25797c8cf67eedae1b168b2484668ffb6a7e0308
Maxime Armstrong <maxime@retain.ai>
2024-08-22 21:04:10 -0400

    Rename command to dagster definitions validate

--- python_modules/dagster/dagster/_cli/__init__.py
+++ python_modules/dagster/dagster/_cli/__init__.py
@@ -3,7 +3,7 @@ import click
 from ..version import __version__
 from .api import api_cli
 from .asset import asset_cli
-from .check import check_command
+from .definitions import definitions_cli
 from .code_server import code_server_cli
 from .debug import debug_cli
 from .dev import dev_command
@@ -28,7 +28,7 @@ def create_dagster_cli():
         "project": project_cli,
         "dev": dev_command,
         "code-server": code_server_cli,
-        "check": check_command,
+        "definitions": definitions_cli,
     }
 
     @click.group(
